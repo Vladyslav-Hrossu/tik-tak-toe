a85f4473f680826d8e0d2b913103e010f8ff29bd
Vladyslav-Hrossu <44838403+Vladyslav-Hrossu@users.noreply.github.com>
2019-05-28 13:16:59 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,117 @@
 let gameField = document.getElementById('game-field');
-let cells = gameField.children;
+let fieldRows = gameField.children;
 let newGame = document.getElementById('new-game');
 let player = document.getElementById('player');
-let gameData = {1:0, 2:0, 3:0,
-                4:0, 5:0, 6:0,
-                7:0, 8:0, 9:0};
-
+const gameData = [];
+const playerOne = {
+    name: 'Player 1',
+    icon: 'X',
+    color: 'red'
+};  
+const playerTwo = {
+    name: 'Player 2',
+    icon: 'O',
+    color: 'green'
+};
+let currentPlayer = playerOne;
 
 newGame.addEventListener('click', startNewGame);
+gameField.addEventListener('click', currentPlayerTurn);
 
-function startNewGame(event) {
-    player.textContent = 'Turn: Player 1';
-    for(let i = 0; i < cells.length; i++){
-        cells[i].textContent = null;
-    }
-    gameData = {1:0, 2:0, 3:0,
-                4:0, 5:0, 6:0,
-                7:0, 8:0, 9:0};
-    gameField.onclick = playerOne;
-    for(let i = 0; i < cells.length; i++){
-        cells[i].style.backgroundColor = 'grey';
-    }
-}
+createGameField(3);
 
-
-
-function playerOne(event){
-    let target = event.target;
-
-    if((!gameData[target.id])) {
-        target.textContent = 'X';
-        gameData[target.id] = 'X';
-        player.textContent = 'Turn: Player 2';
-        target.style.backgroundColor = 'green';
-        gameField.onclick = playerTwo;
-        checkWinner(gameData, 'X');
+function createGameField(fieldSize){
+    for(let i = 0; i < fieldSize; i++){
+        gameData[i] = [];
+        let fieldRow = document.createElement('div');
+        fieldRow.setAttribute('class',`row-${i}`);
+        for(let j = 0; j < fieldSize; j++){
+            gameData[i][j] = null;
+            let fieldCell = document.createElement('div');
+            fieldCell.setAttribute('class',`cell-${j}`);
+            fieldRow.appendChild(fieldCell);
+        }
+        gameField.appendChild(fieldRow);
     }
 }
 
-function playerTwo(event){
-    let target = event.target;
-
-    if((!gameData[target.id])) {
-        target.textContent = 'O';
-        gameData[target.id] = 'O';
-        player.textContent = 'Turn: Player 1';
-        target.style.backgroundColor = 'red';
-        gameField.onclick = playerOne;
-        checkWinner(gameData, 'O');
+function startNewGame() {
+    currentPlayer = playerOne;
+    player.textContent = `Turn: ${currentPlayer.name}`;
+    for(let i = 0; i < fieldRows.length; i++){
+        let fieldCells = fieldRows[i].children;
+        gameData[i] = [];
+        for(let j = 0; j < fieldCells.length; j++){
+            gameData[i][j] = null;
+            fieldCells[j].textContent = null;
+            fieldCells[j].style.backgroundColor = 'white';
+        }
     }
-
 }
-function checkWinner(obj, sym){
-    if((obj[1] === sym && obj[2] === sym && obj[3] === sym) ||
-    (obj[4] === sym && obj[5] === sym && obj[6] === sym) ||
-    (obj[7] === sym && obj[8] === sym && obj[9] === sym) ||
-    (obj[1] === sym && obj[4] === sym && obj[7] === sym) ||
-    (obj[2] === sym && obj[5] === sym && obj[8] === sym) ||
-    (obj[3] === sym && obj[6] === sym && obj[9] === sym) ||
-    (obj[1] === sym && obj[5] === sym && obj[9] === sym) ||
-    (obj[3] === sym && obj[5] === sym && obj[7] === sym)){
-        player.textContent = `${sym} Player Won`;
-        gameField.onclick = '';
 
+function pushData(cell, row){
+    gameData[row.className[4]][cell.className[5]] = currentPlayer.icon;
+}
+
+function togglePlayer() {
+    if(currentPlayer === playerOne) {
+        currentPlayer = playerTwo;
+    } else {
+        currentPlayer = playerOne;
     }
+    player.textContent = `Turn: ${currentPlayer.name}`;
+}
+
+function fillCell(cell) {
+    cell.textContent = currentPlayer.icon;
+    cell.style.backgroundColor = currentPlayer.color;
+}
+
+function currentPlayerTurn(event){
+    let cell = event.target;
+    let row = cell.parentElement;
+    if(!cell.textContent){
+        pushData(cell, row);
+        fillCell(cell);
+        checkWinner(cell, row);
+        togglePlayer();
+    }
+}
+
+function checkWinner(cell, row){
+    let selectedCollumn = +cell.className[5];
+    let selectedRow = +row.className[4];
+    for(let i = 0; i < gameData.length; i++){
+        if(gameData[selectedRow][i] === currentPlayer.icon){
+            if(i === gameData.length - 1) stopGame();
+            continue;
+        }
+        break;
+    }
+    for(let i = 0; i < gameData.length; i++){
+        if(gameData[i][selectedCollumn] === currentPlayer.icon){
+            if(i === gameData.length - 1) stopGame();
+            continue;
+        }
+        break;
+    }
+    for(let i = 0; i < gameData.length; i++){
+        if(gameData[i][i] === currentPlayer.icon){
+            if(i === gameData.length - 1) stopGame();
+            continue;
+        }
+        break;
+    }
+    for(let i = gameData.length - 1, j = 0; i >= 0; i--, j++){
+        if(gameData[j][i] === currentPlayer.icon){
+            if(i === 0) stopGame();
+            continue;
+        }
+        break;
+    }
+}
+
+function stopGame(){
+    gameField.removeEventListener('click', currentPlayerTurn);
+    alert(` ${currentPlayer.name} Won!`);
 }
